b059f964825f5a7a5adb4047678f2c21ae162709
Isha <ishageens@msn.com>
2021-07-01 22:04:27 +0200

    Directives - dropdown close

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,18 +1,12 @@
-import { Directive, HostBinding, HostListener, OnInit } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
-
-export class DropdownDirective implements OnInit {
+export class DropdownDirective {
     @HostBinding('class.open') isOpen = false;
-
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
-    constructor() { }
-    ngOnInit() {
-
-    }
-
+    constructor(private elRef: ElementRef) { }
 }
