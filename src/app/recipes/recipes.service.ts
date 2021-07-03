15fd3edf19fe906703b6ae132f44909168a2eff2
Isha <ishageens@msn.com>
2021-07-03 21:09:55 +0200

    Services

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,18 +1,34 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
+
 export class RecipeService {
+
+    constructor(private slService: ShoppingListService) { }
+
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] =
         [
-            new Recipe('Test Recipe', 'Test', 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F22%2F8000900-2000.jpg '),
-            new Recipe('Another Test Recipe', 'Another Test', 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F22%2F8000900-2000.jpg ')
+            new Recipe('Test Recipe', 'Test', 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F22%2F8000900-2000.jpg ', [
+                new Ingredient('Meat', 1),
+                new Ingredient('French fries', 1)
+            ]),
+            new Recipe('Another Test Recipe', 'Another Test', 'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F22%2F8000900-2000.jpg ', [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
         ];
 
     getRecipes() {
         return this.recipes.slice();
     }
 
+    AddIngredientsToSL(ingredients: Ingredient[]) {
+        this.slService.AddIngredientsList(ingredients);
+    }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,24 @@
-export class ShoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
+export class ShoppingListService {
+    ingredientChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    AddIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
+
+    AddIngredientsList(ingredients: Ingredient[]) {
+        this.ingredients.push(...ingredients);
+        this.ingredientChanged.emit(this.ingredients.slice());
+    }
 }
